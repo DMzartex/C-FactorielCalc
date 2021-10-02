4d9262f907d99887f9fba2fe1b2a6b040d4b3d78
DMzartex <91669692+DMzartex@users.noreply.github.com>
2021-10-02 11:54:23 +0200

    Ajout de FactorielCalc

---
+++ C#DivisionCalc/DivisionCalc/DivisionCalc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DivisionCalc
+{
+    class Program
+    {
+        static void calcFactoriel(int nombre, out string resultat)
+        {
+            resultat = "";
+
+            for (int i = 1; i < nombre; i++)
+            {
+                if((nombre % i) == 0)
+                {
+                    resultat =  resultat + "," +  i;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int nombre;
+            string resultat;
+
+            Console.WriteLine("Entrer votre nombre svp :");
+            nombre = int.Parse(Console.ReadLine());
+
+            calcFactoriel(nombre, out resultat);
+
+            Console.WriteLine(($"Les diviseurs de {nombre} sont : {resultat}"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
